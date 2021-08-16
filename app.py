c59b89f80f40ba55d7d2091da9df3638a3b23e0b
Nithin Lakshmanan <52423467+nithinl@users.noreply.github.com>
2021-08-16 19:40:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,32 +32,6 @@ def create_task():
     return jsonify(
         message="Task saved successfully!"
     )
-@app.route("/task/<id>", methods=["PUT"])
-def update_task(id):
-    data = request.get_json(force=True)["task"]
-    response = db.task.update_one({"_id": ObjectId(id)}, {"$set": {"task": data}})
-    if response.matched_count:
-        message = "Task updated successfully!"
-    else:
-        message = "No Task found!"
-    return jsonify(
-        message=message
-    )
-@app.route("/task/<id>", methods=["DELETE"])
-def delete_task(id):
-    response = db.task.delete_one({"_id": ObjectId(id)})
-    if response.deleted_count:
-        message = "Task deleted successfully!"
-    else:
-        message = "No Task found!"
-    return jsonify(
-        message=message
-    )
-@app.route("/tasks/delete", methods=["POST"])
-def delete_all_tasks():
-    db.task.remove()
-    return jsonify(
-        message="All Tasks deleted!"
-    )
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
